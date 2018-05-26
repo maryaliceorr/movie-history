3bd140901374e6e271ee260a89351ad5986584aa
Mary Alice Orr <m.a.orr17@gmail.com>
2018-05-26 10:04:32 -0500

    added images

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,10 +1,13 @@
-const domString = (movieArray) => {
+const domString = (movieArray, imgConfig) => {
   // need movie array because when domString is called in events.js we are passing in an array of movies
   let strang = '';
-  movieArray.forEach((movie) => {
+  movieArray.forEach((movie, index) => {
+    if (index % 3 === 0) {
+      strang += `<div class="row">`;
+    }
     strang += `<div class="col-sm-6 col-md-4">`;
     strang += `<div class="thumbnail">`;
-    strang += `<img src="..." alt="...">`;
+    strang += `<img src="${imgConfig.base_url}/w342/${movie.poster_path}" alt="Movie Poster">`;
     strang += `<div class="caption">`;
     strang += `<h3>${movie.original_title}</h3>`;
     strang += `<p>${movie.overview}</p>`;
@@ -12,6 +15,10 @@ const domString = (movieArray) => {
     strang += `</div>`;
     strang += `</div>`;
     strang += `</div>`;
+
+    if (index % 3 === 2) {
+      strang += `</div>`;
+    }
   });
   printToDom(strang);
   // put debugger on printToDom to see if array came back do this to make sure all data is there before we add domstring stuff

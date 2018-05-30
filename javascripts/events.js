9617082dc26ebdd8df1ff4b8922b69363c89b430
Mary Alice Orr <m.a.orr17@gmail.com>
2018-05-29 20:03:48 -0500

    added wishlist to firebase

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,4 +1,5 @@
 const tmdb = require('./tmdb');
+const firebaseApi = require('./firebaseApi');
 
 const myLinks = () => {
   $(document).click((e) => {
@@ -28,9 +29,30 @@ const pressEnter = () => {
   });
 };
 
+const saveMovieToWishListEvent = () => {
+  $(document).on('click', '.addMovieToWishList', (e) => {
+    const movieToAddCard = $(e.target).closest('.movie');
+    const movieToAdd = {
+      title: movieToAddCard.find('.movie-title').text(),
+      overview: movieToAddCard.find('.movie-overview').text(),
+      'poster_path': movieToAddCard.find('img').data('poster'),
+      rating: 0,
+      isWatched: false,
+    };
+    firebaseApi.saveMovieToWishList(movieToAdd)
+      .then(() => {
+        movieToAddCard.remove();
+      })
+      .catch ((error) => {
+        console.error('error in saving movie', error);
+      });
+  });
+};
+
 const initializer = () => {
   myLinks();
   pressEnter();
+  saveMovieToWishListEvent();
 };
 module.exports = {
   initializer,

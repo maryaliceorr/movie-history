df5b116c449ba0bcd7e4844649641113d0765426
Mary Alice Orr <m.a.orr17@gmail.com>
2018-06-02 11:09:15 -0500

    got delete button and watchlist working

--- javascripts/events.js
+++ javascripts/events.js
@@ -75,11 +75,33 @@ const saveMovieToWishListEvent = () => {
   });
 };
 
+const updateMovieEvent = () => {
+  $(document).on('click', '.updateMovieToWatched', (e) => {
+    const movieToUpdateId = $(e.target).closest('.movie').data('firebaseId');
+    const movieToUpdateCard = $(e.target).closest('.movie');
+    const updatedMovie = {
+      title: movieToUpdateCard.find('.movie-title').text(),
+      overview: movieToUpdateCard.find('.movie-overview').text(),
+      'poster_path': movieToUpdateCard.find('img').data('poster'),
+      rating: 0,
+      isWatched: true,
+    };
+    firebaseApi.updateMovieToWatchedInDb(updatedMovie, movieToUpdateId)
+      .then(() => {
+        getAllMoviesEvent();
+      })
+      .catch((error) => {
+        console.error('got error when updating movie', error);
+      });
+  });
+};
+
 const initializer = () => {
   myLinks();
   pressEnter();
   saveMovieToWishListEvent();
   deleteMovieFromFirebase();
+  updateMovieEvent();
 };
 module.exports = {
   initializer,

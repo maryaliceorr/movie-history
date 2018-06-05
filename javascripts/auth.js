bf58ac476038feb789c995c0e4529c9d2b813178
Mary Alice Orr <m.a.orr17@gmail.com>
2018-06-04 21:27:15 -0500

    figured got authentication and registration working

---
+++ javascripts/auth.js
@@ -0,0 +1,29 @@
+const {getAllMoviesEvent,} = require('./events');
+
+const checkLoginStatus = () => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // User is signed in.
+      $('#myMovies').removeClass('hide');
+      $('#search').addClass('hide');
+      $('#authScreen').addClass('hide');
+      $('#mine, #navSearch, #logout').removeClass('hide');
+      $('#authenticate').addClass('hide');
+      // call the getMoviesEvent
+      getAllMoviesEvent();
+    } else {
+      // No user is signed in.
+      $('#myMovies').addClass('hide');
+      $('#search').addClass('hide');
+      $('#authScreen').removeClass('hide');
+      $('#logout, #mine, #navSearch').addClass('hide');
+      $('#authenticate').removeClass('hide');
+    }
+  });
+};
+
+// putting this function here allows people logged in to come to the same page
+
+module.exports = {
+  checkLoginStatus,
+};
